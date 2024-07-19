6b84f6353fd2d19e7ce00f4f60345d97170da47d
ShazaibAbuBasharMernStackdeveloper2210 <142742492+shazaibabubashar2210@users.noreply.github.com>
2024-07-19 23:00:16 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+var modal = document.getElementById("modal");
+
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("caption");
+
+document.querySelectorAll('.gallery-item img').forEach(img => {
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+modal.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
